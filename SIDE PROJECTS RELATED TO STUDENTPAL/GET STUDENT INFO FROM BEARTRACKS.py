8622d963f5acb852f0a6c44722938d25333d54dd
Abdulhafiz Dada <dadaabdulhafiz0306@gmail.com>
2024-04-05 17:35:13 -0600

    More work

--- SIDE PROJECTS RELATED TO STUDENTPAL/GET STUDENT INFO FROM BEARTRACKS.py
+++ SIDE PROJECTS RELATED TO STUDENTPAL/GET STUDENT INFO FROM BEARTRACKS.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 from bs4 import BeautifulSoup
+from selenium.webdriver.common.action_chains import ActionChains
 
 url = "https://www.beartracks.ualberta.ca/"
 
@@ -31,26 +32,43 @@ def openProfile():
     login = driver.find_element(By.XPATH, "//*[@id='loginform']/input[3]")
     login.click()
         
-    profile = WebDriverWait(driver, 20).until(
+    profile = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.ID, "win2div$ICField3"))
     )
     
     profile.click()
-    time.sleep(3)
+
+    WebDriverWait(driver, 10).until(
+    EC.visibility_of_element_located((By.ID, "HCR_PERSON_I_BIRTHDATE"))
+)
+
 
     soup = BeautifulSoup(driver.page_source, "html.parser")
-    DOBEL = soup.find(id = "HCR_PERSON_I_BIRTHDATE")
+    DOB = soup.find(id = "HCR_PERSON_I_BIRTHDATE")
     Gender = soup.find(id = "HCR_PER_PDE_I_SEX")
     Country = soup.find(id = "ZCCV_CITIZEN_VW_DESCR1")
     Status = soup.find(id = "ZCCV_CITIZEN_VW_DESCR")
     SIN = soup.find(id = "SCC_PROF_FL_DRV_NATIONAL_ID")
-    info["DateOfBirth"] = DOBEL.text
+    info["DateOfBirth"] = DOB.text
     info["Gender"] = Gender.text
     info["Country"] = Country.text
     info["Status"] = Status.text
     info["SIN"] = SIN.text
-    
-    
+
+    nameSect = driver.find_element(By.XPATH, "//*[@id='SCC_NAMES_H$0_row_0']/td[1]")
+    ActionChains(driver).move_to_element(nameSect).perform()
+    nameSect.click()
+
+    WebDriverWait(driver, 10).until(
+    EC.visibility_of_element_located((By.XPATH, "//*[@id='HCR_UPDNMFL_DVW_FIRST_NAME$1']"))
+)
+
+    soup = BeautifulSoup(driver.page_source, "html.parser")
+    firstName = soup.find(id = "HCR_UPDNMFL_DVW_FIRST_NAME$1").text
+    middleName = soup.find(id = "HCR_UPDNMFL_DVW_MIDDLE_NAME$2").text
+    lastName = soup.find(id = "HCR_UPDNMFL_DVW_MIDDLE_NAME").text
+
+    info["Name"] = f"{firstName} {middleName} {lastName}"
 
 openProfile()
 print(info)
