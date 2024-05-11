a01fe747c328a68627ef23f2fd9f46fe591c6027
Abdulhafiz Dada <dadaabdulhafiz0306@gmail.com>
2024-05-10 23:04:00 -0600

    Beginning stage completed.

--- IN PROGRESS/DAILY WEATHER TEXT/OpenMeteo.py
+++ IN PROGRESS/DAILY WEATHER TEXT/OpenMeteo.py
@@ -1,14 +1,14 @@
 import openmeteo_requests
-
 import requests_cache
 from retry_requests import retry
 import json
+from twilio.rest import Client
+
 
 def getWeatherCode(latitude, longitude):
 	cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
 	retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
 	openmeteo = openmeteo_requests.Client(session = retry_session)
-
 
 	url = "https://api.open-meteo.com/v1/forecast"
 	params = {
@@ -18,8 +18,7 @@ def getWeatherCode(latitude, longitude):
 		"timezone": "auto",
 		"forecast_days": 1
 	}
-	responses = openmeteo.weather_api(url, params=params)
-
+	responses = openmeteo.weather_api(url, params=params)	
 	response = responses[0]
 
 	daily = response.Daily()
@@ -35,8 +34,21 @@ def interpretWeatherCode(code):
 	night = codeDict[str(int(code))]["night"]["description"]
 	return day, night
 
-timezone, timezoneAbbrev, weather_code = getWeatherCode(53.5501, -113.4687)
-print(f"Timezone {timezone} {timezoneAbbrev}")
+def sendText(recepient, messageSend):
+	account_sid = "AC07ad148158c8b75259fb7eabb9669039"
+	auth_token = "5d598d576d3a238634a1008e5cb3fdb0"
+	client = Client(account_sid, auth_token)
 
+	message = client.messages \
+					.create(
+						body= messageSend,
+						from_='+13343264571',
+						to= recepient
+					)
+
+timezone, timezoneAbbrev, weather_code = getWeatherCode(53.5501, -113.4687)
 day, night= interpretWeatherCode(weather_code[0])
-print(day, night)
+
+messageText = f"Today in {timezone} the weather is going to be {day}. At night, it will be {night}! Dress for the weather. :)"
+
+sendText("+17803990244", messageText)
