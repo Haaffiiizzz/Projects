07292f25dcb0c81e43caf28bb3fec9d4dfd4a865
Abdulhafiz Dada <dadaabdulhafiz0306@gmail.com>
2024-05-19 23:22:15 -0600

    Great start

--- IN PROGRESS/COMMON ITEM-CURRENCY COMPARER/Currency Converter.py
+++ IN PROGRESS/COMMON ITEM-CURRENCY COMPARER/Currency Converter.py
@@ -1,7 +1,54 @@
-def getCurrencyValues(firstCurr, secondCurr):
+import json
+ratesFile = open(r"IN PROGRESS\COMMON ITEM-CURRENCY COMPARER\rates.json", "r")
+ratesDict = json.load(ratesFile)
+
+itemsFile  = open(r"IN PROGRESS\COMMON ITEM-CURRENCY COMPARER\items.json", "r")
+itemsDict = json.load(itemsFile)
+
+def getCurrencyValues(baseCurr):
+    # access how much 1 dollar is in base currency e.g 1000 naira
+    return ratesDict[baseCurr]
+
+def calculateComparedUnits(baseCurr, baseUnits, comparedCurr):
+    # calculate how much in compared currency a unit of base currency is
+    # for example 5 units of dollar * 1000 naira so 5000 naira
+    comparedUnits = ((1 / ratesDict[baseCurr]) *  baseUnits) * ratesDict[comparedCurr]
+    return comparedUnits
+
+def createDict(comparedCurr, comparedUnit):
+    # retrieves the dictionary for the compared country's item and adjusts accordingly, returns
+    # a dictionary with items and how many base currency can buy
+
+    comparedDict = itemsDict[comparedCurr]
+    for key, value in comparedDict.items():
+        units = round(comparedUnit/value)
+        comparedDict[key] = units
+
+    return comparedDict
+
+def printComparedItems(baseCurr, baseUnits, comparedCurr, comparedDict):
+    
+    print(f"\nThis is what {baseUnits} {baseCurr} can buy in {comparedCurr} country:\n")
+
+    for key, value in comparedDict.items():
+        print(f"{value} {key.title()}")
+
 
 def main():
+    base = input("\nType in the base currency, leave a space then type in how many units:\n").split(" ")
+    baseCurr = base[0].upper()
+    baseUnits = int(base[1])
+
+    comparedCurr = input("\nType in the compared country's currency:\n").upper()
+    comparedUnits = calculateComparedUnits(baseCurr, baseUnits, comparedCurr)
+    comparedDict = createDict(comparedCurr, comparedUnits)
     
+    printComparedItems(baseCurr, baseUnits, comparedCurr, comparedDict)
+
+if __name__ == "__main__":
+    main()
+
+
 
 
 
