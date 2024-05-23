e8af1ed5774a7f25b87d75b2fee2d1d6fcd354b1
Abdulhafiz Dada <dadaabdulhafiz0306@gmail.com>
2024-05-22 22:46:38 -0600

    new file and json for rates

---
+++ IN PROGRESS/COMMON ITEM-CURRENCY COMPARER/currency rates.py
@@ -0,0 +1,12 @@
+import json
+import requests
+
+# request for the rates from the api and returns the json file
+response = requests.get("https://v6.exchangerate-api.com/v6/b11b7775a7d823efffd22253/latest/USD")
+if response.status_code == 200:
+    ratesDict = response.json()["conversion_rates"]
+else:    
+    raise Exception("Not 200 code")
+
+with open(r"IN PROGRESS\COMMON ITEM-CURRENCY COMPARER\rates.json", "w") as file:
+    json.dump(ratesDict, file, indent=2)
