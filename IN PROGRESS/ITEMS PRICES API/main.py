9933c58581c8032fe8dd19e5f74ab2f3be4230e8
Abdulhafiz Dada <dadaabdulhafiz0306@gmail.com>
2024-05-31 16:54:07 -0600

    Can now post new prices to a country

--- IN PROGRESS/ITEMS PRICES API/main.py
+++ IN PROGRESS/ITEMS PRICES API/main.py
@@ -1,5 +1,4 @@
-from fastapi import FastAPI
-from fastapi.params import Body
+from fastapi import FastAPI, status, HTTPException, Body
 from pydantic import BaseModel
 import json
 app = FastAPI()
@@ -10,14 +9,14 @@ data = json.load(file)
 class AddData(BaseModel):
     #  this makes sure we are getting the right data format else it
     # throws and error
-    country: str
-    price: dict
+    prices: dict
 
 
 @app.get("/")
 def root():
     # this is the base site without any paths
     return {"message": "Welcome to my API"}
+
 
 @app.get("/countries")
 def getPrices():
@@ -29,13 +28,25 @@ def getPrices():
 def getCountryPrices(country: str):
     #  in this path we should return a json of just a country
     #  and its items and prices
+    if country not in data:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"{country} not found")
     return {"country": data[country]}
 
-@app.post("/addprice")
-def addPrice(newData: AddData):
+
+@app.post("/countries/{country}")
+def addPrice(country, newData: AddData = Body(...)):
     #  first check to make sure we have the right data format
     #  send back to user and print data
-    print(newData)
-    return {"added price": newData}
+    if country.title() not in data:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"{country} not found")
+    
+    data[country.title()][1].update(newData.prices)    # update the country's price dict with the new items
+    
+    with open(r"C:\Users\dadaa\Projects\IN PROGRESS\ITEMS PRICES API\countries.json", "w") as file:
+        json.dump(data, file, indent=4)
+
+    return {"Added price": {"Country" : country.title(), "items": newData.price}}
 
 
