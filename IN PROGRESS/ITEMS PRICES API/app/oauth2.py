fc1388ff08019d435bcaa8d814f85c2e7332d70a
Abdulhafiz Dada <dadaabdulhafiz0306@gmail.com>
2024-06-11 14:01:30 -0600

    user login

--- IN PROGRESS/ITEMS PRICES API/app/oauth2.py
+++ IN PROGRESS/ITEMS PRICES API/app/oauth2.py
@@ -1,2 +1,15 @@
 from jose import JWTError, jwt
+from datetime import datetime, timedelta
 
+SECRET_KEY = "09d25e094faa6ca2556c81816637a9563b93f7099f6f0f4caa6cf63b88e8d3h7"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+def createToken(data: dict):
+    toEncode= data.copy()
+
+    expire = datetime.now() + timedelta(minutes= ACCESS_TOKEN_EXPIRE_MINUTES)
+    toEncode.update({"exp": expire})
+
+    encodedJWT = jwt.encode(toEncode, SECRET_KEY, algorithm = ALGORITHM)
+    return encodedJWT
--- IN PROGRESS/ITEMS PRICES API/app/routers/auth.py
+++ IN PROGRESS/ITEMS PRICES API/app/routers/auth.py
@@ -2,13 +2,16 @@ from fastapi import APIRouter, Depends, status, HTTPException, Response
 from sqlalchemy.orm import Session
 from database import get_db
 from schemas import UserLogin
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 import models
 import utils
+import oauth2
+
 router = APIRouter(tags = ["Authentication"])
 
 @router.post("/login")
-def login(user_credentials: UserLogin, db: Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.email == user_credentials.email).first()
+def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
 
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
@@ -18,4 +21,5 @@ def login(user_credentials: UserLogin, db: Session = Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"Invalid Credentials")
     
-    return {"token": "example token"}
+    accessToken = oauth2.createToken(data = {"user_id": user.id})
+    return {"Access Token": accessToken, "Token type": "bearer"}
